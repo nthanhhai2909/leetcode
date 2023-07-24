debd354757338cdf98197d06f6816d1416e61d0d
hai.t.nguyen <hai.t.nguyen@opswat.com>
2023-07-24 22:19:45 +0700

    powx - v1 and v2

---
+++ medium/000-000-004.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+/*
+Leetcode: https://leetcode.com/problems/powx-n/
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+*/
+
+func myPowV1(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	if n < 0 {
+		return 1 / myPowV1(x, -n)
+	}
+	return x * myPowV1(x, n-1)
+}
+
+func myPowV2(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	if n < 0 {
+		return 1 / myPowV2(x, -n)
+	}
+	if n%2 == 0 {
+		return myPowV2(x*x, n/2)
+	} else {
+		return x * myPowV2(x*x, (n-1)/2)
+	}
+}
+
+func main() {
+	fmt.Println(myPowV2(2.00000, -10))
+}
