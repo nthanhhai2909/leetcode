80d649cadd66d871d4dc4b3d03dd20a2ed5e07be
hai.t.nguyen <hai.t.nguyen@opswat.com>
2024-01-22 17:18:13 +0700

    Minimum Number of Moves to Seat Everyone

---
+++ easy/000-000-007.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/*
+*
+
+Link: https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/description/
+2037. Minimum Number of Moves to Seat Everyone
+*/
+
+func minMovesToSeat(seats []int, students []int) int {
+	n := len(seats)
+	if n == 0 {
+		return 0
+	}
+
+	sort.Slice(seats, func(i, j int) bool { return seats[i] < seats[j] })
+	sort.Slice(students, func(i, j int) bool { return students[i] < students[j] })
+
+	ans := 0
+	for i := 0; i < n; i++ {
+		ans += abs(seats[i] - students[i])
+	}
+
+	return ans
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+// ------------------------------v------------------------------------------------------------
+
+func main() {
+
+	seats := []int{3, 1, 5}
+	students := []int{2, 7, 4}
+
+	fmt.Println(minMovesToSeat(seats, students))
+	fmt.Println("go again!")
+}
